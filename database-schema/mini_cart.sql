d7c885ad11efa48d35a7731aec715a8e6a22512b
Maneesha Indrachapa <maneesh.15@cse.mrt.ac.lk>
2023-07-05 19:31:56 +0200

    db scema

--- database-schema/mini_cart.sql
+++ database-schema/mini_cart.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 31, 2022 at 07:57 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.0.19
+-- Generation Time: Jul 05, 2023 at 07:31 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,34 +24,12 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `accounts`
---
-
-CREATE TABLE `accounts` (
-  `id` int(11) NOT NULL,
-  `name` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `type_id` int(11) NOT NULL,
-  `created_date` datetime NOT NULL,
-  `active` tinyint(1) DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `accounts`
---
-
-INSERT INTO `accounts` (`id`, `name`, `type_id`, `created_date`, `active`) VALUES
-(1, 'travel goods', 2, '2022-04-30 14:37:13', 1),
-(2, 'building materials', 2, '2022-04-30 14:37:13', 1);
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `account_types`
 --
 
 CREATE TABLE `account_types` (
   `id` int(11) NOT NULL,
-  `name` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(45) NOT NULL,
   `price` double NOT NULL,
   `level` int(11) NOT NULL,
   `max_warehouses` int(11) NOT NULL,
@@ -72,10 +50,10 @@ CREATE TABLE `account_types` (
 
 CREATE TABLE `addresses` (
   `id` int(10) UNSIGNED NOT NULL,
-  `country` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `city` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `address` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `zip` varchar(11) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `zip` varchar(11) DEFAULT NULL,
   `latitude` float(10,8) DEFAULT NULL,
   `longitude` float(11,8) DEFAULT NULL,
   `warehouse_id` int(10) UNSIGNED DEFAULT NULL,
@@ -91,26 +69,12 @@ CREATE TABLE `addresses` (
 CREATE TABLE `associates` (
   `id` int(10) UNSIGNED NOT NULL,
   `account_id` int(11) NOT NULL,
-  `name` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `phone` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `additional_info` mediumtext COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `type` enum('CLIENT','SUPPLIER') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `name` varchar(45) NOT NULL,
+  `email` varchar(60) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `additional_info` mediumtext DEFAULT NULL,
+  `type` enum('CLIENT','SUPPLIER') DEFAULT NULL,
   `active` tinyint(1) DEFAULT 1
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `cart`
---
-
-CREATE TABLE `cart` (
-  `cart_id` int(200) NOT NULL,
-  `user_id` int(200) NOT NULL,
-  `item_id` int(100) NOT NULL,
-  `quantity` int(10) NOT NULL,
-  `price` decimal(20,0) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -121,12 +85,12 @@ CREATE TABLE `cart` (
 
 CREATE TABLE `events` (
   `id` int(10) UNSIGNED NOT NULL,
-  `message` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `message` text NOT NULL,
   `date` datetime NOT NULL DEFAULT current_timestamp(),
   `account_id` int(11) NOT NULL,
   `warehouse_id` int(11) DEFAULT NULL,
   `author_id` int(11) NOT NULL,
-  `name` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(32) NOT NULL,
   `transaction_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -137,24 +101,24 @@ CREATE TABLE `events` (
 --
 
 CREATE TABLE `items` (
-  `id` bigint(25) NOT NULL,
-  `name_item` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `price` decimal(10,2) NOT NULL,
-  `unit` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` varchar(10000) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` bigint(20) NOT NULL,
+  `name_item` varchar(255) NOT NULL,
+  `unit` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
   `volume` int(11) NOT NULL,
   `active` tinyint(4) NOT NULL,
   `account_id` bigint(20) NOT NULL,
-  `image_url` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL
+  `price` double NOT NULL,
+  `image_url` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `items`
 --
 
-INSERT INTO `items` (`id`, `name_item`, `price`, `unit`, `description`, `volume`, `active`, `account_id`, `image_url`) VALUES
-(5, 'cake', '25.52', '50', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 123, 0, 0, 'https://www.edeka.de/media/01-rezeptbilder/rezeptbilder-a-d/rez-edeka-burger-the-big-american-rezept-a-d-1-1.jpg'),
-(11, 'ds', '12.00', '11', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 111, 0, 0, 'https://www.edeka.de/media/01-rezeptbilder/rezeptbilder-a-d/rez-edeka-burger-the-big-american-rezept-a-d-1-1.jpg');
+INSERT INTO `items` (`id`, `name_item`, `unit`, `description`, `volume`, `active`, `account_id`, `price`, `image_url`) VALUES
+(8, 'Sahana Cake', '19', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua', 0, 0, 0, 1000, 'https://iheartcraftythings.com/wp-content/uploads/2021/07/6-38.jpg'),
+(19, 'zzz Cake', '16', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua', 0, 0, 0, 25, 'https://iheartcraftythings.com/wp-content/uploads/2021/07/6-38.jpg');
 
 -- --------------------------------------------------------
 
@@ -178,18 +142,10 @@ CREATE TABLE `saved_items` (
 CREATE TABLE `transactions` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
-  `user_id` bigint(20) UNSIGNED NOT NULL,
-  `item_id` bigint(25) UNSIGNED NOT NULL,
+  `worker_id` int(10) UNSIGNED NOT NULL,
+  `item_id` int(10) UNSIGNED NOT NULL,
   `quantity` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `transactions`
---
-
-INSERT INTO `transactions` (`id`, `timestamp`, `user_id`, `item_id`, `quantity`) VALUES
-(1, '2022-05-18 10:57:38', 2, 5, 10),
-(2, '2022-04-18 10:57:38', 2, 5, 40);
 
 -- --------------------------------------------------------
 
@@ -199,25 +155,28 @@ INSERT INTO `transactions` (`id`, `timestamp`, `user_id`, `item_id`, `quantity`)
 
 CREATE TABLE `users` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `first_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `last_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `role` enum('ADMIN','WORKER','CUSTA') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `role` enum('ADMIN','CASH','INVM') DEFAULT NULL,
   `created_date` datetime NOT NULL,
   `updated_date` datetime NOT NULL,
   `active` tinyint(1) DEFAULT 0,
-  `account_id` int(11) DEFAULT NULL
+  `email_uuid` varchar(64) NOT NULL,
+  `account_id` int(11) DEFAULT NULL,
+  `forget_password_code` int(8) DEFAULT NULL,
+  `validate_forget_password_code` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `password`, `role`, `created_date`, `updated_date`, `active`, `account_id`) VALUES
-(1, 'Maneesha', 'Ewing', 'x@gmail.com', '123123', 'ADMIN', '2022-05-18 00:00:00', '2021-06-02 14:39:05', 0, 1),
-(2, 'Allegra', 'Alexander', 'y@gmail.com', '123123', 'CUSTA', '2021-06-02 14:39:05', '2021-06-02 14:39:05', 1, 1),
-(3, 'Akeem', 'Galloway', 'eros.non@google.org', '123123', 'CUSTA', '2021-06-02 14:39:05', '2021-06-02 14:39:05', 1, 1);
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `password`, `role`, `created_date`, `updated_date`, `active`, `email_uuid`, `account_id`, `forget_password_code`, `validate_forget_password_code`) VALUES
+(5, 'Maneesha', 'Indrachapa', 'maneeshaindrachapa@gmail.com', '123123', 'ADMIN', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, '', NULL, 318874, 0),
+(15, 'Maneesha', 'Indrachapa', 'cloudpiercer09@gmail.com', '123123', 'CASH', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, '', NULL, NULL, 0),
+(16, 'Maneesha', 'kat', 'buddhini.kat95@gmail.com', '123123', 'INVM', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, '', NULL, NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -227,8 +186,8 @@ INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `password`, `role
 
 CREATE TABLE `warehouses` (
   `id` int(10) UNSIGNED NOT NULL,
-  `name` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `info` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `name` varchar(45) NOT NULL,
+  `info` varchar(100) DEFAULT NULL,
   `capacity` int(10) UNSIGNED DEFAULT NULL,
   `is_bottom` tinyint(1) NOT NULL,
   `parent_id` int(10) UNSIGNED DEFAULT NULL,
@@ -240,12 +199,6 @@ CREATE TABLE `warehouses` (
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `accounts`
---
-ALTER TABLE `accounts`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `account_types`
@@ -287,8 +240,7 @@ ALTER TABLE `saved_items`
 -- Indexes for table `transactions`
 --
 ALTER TABLE `transactions`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `transactions_ibfk_1` (`user_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -307,12 +259,6 @@ ALTER TABLE `warehouses`
 --
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `accounts`
---
-ALTER TABLE `accounts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `account_types`
@@ -342,7 +288,7 @@ ALTER TABLE `events`
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `id` bigint(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `saved_items`
@@ -354,23 +300,13 @@ ALTER TABLE `saved_items`
 -- AUTO_INCREMENT for table `transactions`
 --
 ALTER TABLE `transactions`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `transactions`
---
-ALTER TABLE `transactions`
-  ADD CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
